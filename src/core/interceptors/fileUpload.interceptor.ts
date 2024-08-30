a0c5b72c3d248d404b2b02208ae0643884d56a6b
Pham Anh Tuan <tuanpa@MECORP.LOCAL>
2024-08-30 15:51:23 +0700

    draft minio service

--- src/core/interceptors/fileUpload.interceptor.ts
+++ src/core/interceptors/fileUpload.interceptor.ts
@@ -3,8 +3,37 @@ import { Observable } from "rxjs";
 
 @Injectable()
 export class FileUploadInterceptor implements NestInterceptor {
-    
-    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-        storage
+    async intercept(
+        context: ExecutionContext,
+        next: CallHandler,
+    ): Promise<Observable<any>> {
+        const request = context.switchToHttp().getRequest<Request>();
+
+        // Assuming you're uploading the file via `multipart/form-data`
+        const file = request.file; // For single file
+        // const files = request.files; // For multiple files
+
+        if (file) {
+            // Ensure the bucket exists or create it
+            const bucketName = 'my-bucket';
+            await this.minioService.ensureBucketExists(bucketName);
+
+            // Upload the file to Minio
+            const objectName = file.originalname;
+            const fileBuffer = file.buffer;
+            const contentType = file.mimetype;
+
+            await this.minioService.uploadFile(
+                bucketName,
+                objectName,
+                fileBuffer,
+                contentType,
+            );
+
+            // Add file URL to the request or response if necessary
+            request.body.fileUrl = `http://YOUR_MINIO_ENDPOINT/${bucketName}/${objectName}`;
+        }
+
+        return next.handle();
     }
 }
--- src/infras/object-storage/minio.service.ts
+++ src/infras/object-storage/minio.service.ts
@@ -1,6 +1,34 @@
 import { Injectable } from "@nestjs/common";
-
+import * as Minio from 'minio';
 @Injectable()
 export class MinioService {
-    private readonly miniof
+    private readonly minioClient: Minio.Client;
+
+    constructor() {
+        this.minioClient = new Minio.Client({
+            endPoint: process.env.MINIO_URL,
+            port: Number.parseInt(process.env.MINIO_PORT),
+            useSSL: process.env.NODE_ENV === 'production',
+            accessKey: process.env.MINIO_ACCESS_KEY,
+            secretKey: process.env.MINIO_SECRET_KEY,
+        });
+    }
+
+    async uploadFile(file: Express.Multer.File, bucketName: string, region?: string): Promise<void> {
+        const existedBucket = await this.minioClient.bucketExists(bucketName);
+        if (!existedBucket)
+            await this.minioClient.makeBucket(bucketName, region);
+
+        const metaData = {
+            'Content-Type': file.mimetype,
+        };
+
+        await this.minioClient.putObject(
+            bucketName,
+            file.originalname,
+            file.buffer,
+            file.size,
+            metaData,
+        );
+    }
 }
