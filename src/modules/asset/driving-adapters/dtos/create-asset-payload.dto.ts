e94f5180cbe09ee25b7b337099a2e3cf7b5b36ab
phamanhtuanevol <anhtuan@evol.vn>
2025-06-25 22:09:16 +0700

    Changes to be committed: 	deleted:    .github/workflows/cd.yml 	modified:   .github/workflows/ci.yml 	deleted:    .github/workflows/release.yml 	modified:   src/modules/asset/driving-adapters/dtos/create-asset-payload.dto.ts

--- src/modules/asset/driving-adapters/dtos/create-asset-payload.dto.ts
+++ src/modules/asset/driving-adapters/dtos/create-asset-payload.dto.ts
@@ -1,10 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { ASSET_TYPE } from '@modules/asset/domain/entities/asset/asset-type.value-object';
 import { MIME_TYPE } from '@modules/asset/domain/entities/asset/mime-type.value-object';
 
 export class CreateAssetPayloadDTO {
+    @ApiProperty({
+        type: 'string',
+        format: 'binary',
+        description: 'File buffer data',
+        example: 'Binary file data'
+    })
     fileBuffer: Buffer;
+
+    @ApiProperty({
+        type: 'string',
+        description: 'Original filename',
+        example: 'avatar.jpg'
+    })
     fileName: string;
+
+    @ApiProperty({
+        type: 'number',
+        description: 'File size in bytes',
+        example: 1024
+    })
     fileSize: number;
+
+    @ApiProperty({
+        enum: MIME_TYPE,
+        description: 'MIME type of the file',
+        example: MIME_TYPE.GIF
+    })
     mimeType: MIME_TYPE;
+
+    @ApiProperty({
+        enum: ASSET_TYPE,
+        description: 'Type of asset',
+        example: ASSET_TYPE.IMAGE
+    })
     assetType: ASSET_TYPE;
 }
